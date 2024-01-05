b82164a9f2533732cfef49ca40f6044803fead15
NaveenShet <naveenrvshet5@gmail.com>
2024-01-05 23:50:45 +0530

    add functionalities to components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((preNotes) => {
+      return [...preNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((preNotes) => {
+      return preNotes.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => (
+      <CreateArea onAdd={addNote} />
+
+      {notes.map((noteItem, index) => (
         <Note
-          key={noteItem.id}
+          onDelete={deleteNote}
+          key={index}
+          id={index}
           title={noteItem.title}
           content={noteItem.content}
         />
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setNote((preNote) => {
+      return {
+        ...preNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({ title: "", content: "" });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={handleChange}
+          value={note.title}
+          name="title"
+          placeholder="Title"
+        />
+
+        <textarea
+          onChange={handleChange}
+          value={note.content}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
+
+        <button type="submit" onClick={submitNote}>
+          ADD
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
